f81233d7413f66f577c34af5d38f619ee493873b
pjkate52 <pjkate52@gmail.com>
2026-04-25 07:08:12 +0000

    d

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,10 +1,6 @@
-# a = input()
-# if '.' in a:
-#     print("This number is a decimal.")
-# else:
-#     print("This number is a integer.")
-a = (input())
-if '.00' in a:
-    print("This number is a integer.")
-else:
+num = input("Give me a number: ")
+
+if not float(num).is_integer():
     print("This number is a decimal.")
+else:
+    print("This number is an integer.")
