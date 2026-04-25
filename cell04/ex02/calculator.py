5804fc683133a3dd7cf1d6f462a4390beaa39d1b
pjkate52 <pjkate52@gmail.com>
2026-04-25 05:59:43 +0000

    d

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+a = int(input("Give me the first number: "))
+b = int(input("Give me the second number: "))
+
+print("thank you !")
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} x {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
